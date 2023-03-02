871ab48964e8d610da97651e5a3c50347f15a66f
ali <ali_ramazanov_2000@mail.ru>
2023-03-02 19:43:29 +0300

    real solution and thomasa

---
+++ diff_con.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sys import stderr
+import time
+
+def real_solitoin(x, Pe = 1):
+    return (np.exp(Pe * x) - 1.0) / (np.exp(Pe) - 1.0)
+
+def thomas_solver(n, d, du, dl, b):
+    if d[0] == 0:
+        stderr.write('condition w[i]==0 not met')
+        exit(-1)
+    q = np.zeros(n)
+    g = np.zeros(n)
+
+    u = np.zeros(n)
+    q[0] = du[0]/d[0]
+    g[0] = b[0] / d[0]
+    for i in range(1, n):
+        w = d[i] - dl[i] * q[i - 1]
+        if w == 0:
+            stderr.write('condition w[i]==0 not met')
+            exit(-1)
+        if i != n-1:
+            q[i] = du[i]/w
+        g[i] = (b[i] - dl[i] * g[i-1]) / w
+
+    u[n - 1] = g[n - 1]
+    for i in range(n - 2, -1, -1):  # i = N-2, N-1, ... , 0
+        u[i] = g[i] - q[i] * u[i + 1]
+
+    return u
+
+
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    main()
